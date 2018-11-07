6f769af57f8b0c93b086fe531c6902679665fc1f
zhen-ke <cool420984@gmail.com>
2018-11-07 23:40:22 +0800

    启动阿里移动端适配方案

--- src/style.js
+++ src/style.js
@@ -103,9 +103,6 @@ export const GlobalStyle = createGlobalStyle`
   input,textarea,button{
     -webkit-appearance: none;
   }
-  html {
-    font-size:calc(100vw / 7.5)
-  }
   body {
     font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
     color: #333;
