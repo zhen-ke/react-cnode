ac507479c66726d71924d0ea0c41ac7fb18d2c87
juemingzi <juemingzi@dianjia.io>
2019-04-02 11:51:36 +0800

    添加鉴权方案

--- src/common/auth/index.js
+++ src/common/auth/index.js
@@ -1,37 +1,107 @@
 import React, { Component } from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
+import { actionCreators as loginActionCreators } from "./../login/store";
 
-class Auth extends Component {
-  render() {
-    let { isLogined } = this.props;
-    return this.AuthFun({
-      component: this.props.component,
-      isLogined,
-      ...this.props
-    });
-  }
-  AuthFun({ component: Component, isLogined, ...rest }) {
-    let isLogin = localStorage.user || isLogined;
-    return (
-      <Route
-        {...rest}
-        render={props =>
-          isLogin ? (
-            <Component {...props} />
-          ) : (
-            <Redirect
-              to={{
-                pathname: "/login",
-                state: { from: props.location }
-              }}
-            />
-          )
-        }
-      />
-    );
-  }
-}
+// 鉴权方案一
+// class PriveateRoute extends Component {
+//   render() {
+//     let { isLogined } = this.props;
+//     return this.AuthFun({
+//       component: this.props.component,
+//       isLogined,
+//       ...this.props
+//     });
+//   }
+//   AuthFun({ component: Component, isLogined, ...rest }) {
+//     let isLogin = localStorage.user || isLogined;
+//     return (
+//       <Route
+//         {...rest}
+//         render={props =>
+//           isLogin ? (
+//             <Component {...props} />
+//           ) : (
+//             <Redirect
+//               to={{
+//                 pathname: "/login",
+//                 state: { from: props.location }
+//               }}
+//             />
+//           )
+//         }
+//       />
+//     );
+//   }
+// }
+
+// 鉴权方案二
+// class PriveateRoute extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isLogin: false
+//     };
+//   }
+//   componentDidMount() {
+//     this.getAuth();
+//   }
+//   render() {
+//     let { component: Component, path = "/" } = this.props;
+//     let { isLogin } = this.state;
+//     return isLogin ? (
+//       <Route path={path} render={props => <Component {...props} />} />
+//     ) : (
+//       "请重新登入"
+//     );
+//   }
+//   getAuth() {
+//     let { isLogined } = this.props;
+//     let isLogin = localStorage.user || isLogined;
+//     this.setState({
+//       isLogin
+//     });
+//     if (!isLogin) {
+//       const { history } = this.props;
+//       history.replace("/login");
+//       // setTimeout(() => {
+//       //   history.replace("/login");
+//       // }, 1000);
+//     }
+//   }
+// }
+
+// 鉴权方案三
+const WithPriveateRoute = WrappedComponent => {
+  return class HOC extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        isLogin: false
+      };
+    }
+    componentDidMount() {
+      this.getAuth();
+    }
+    render() {
+      const { pathname } = this.props.location;
+      this.props.savePath(pathname)
+      return <WrappedComponent {...this.props} />;
+    }
+    getAuth() {
+      let { isLogined } = this.props;
+      let isLogin = localStorage.user || isLogined;
+      this.setState({
+        isLogin
+      });
+      if (!isLogin) {
+        const { history } = this.props;
+        history.replace("/login");
+      }
+    }
+  };
+};
+const PriveateRoute = WithPriveateRoute(Route);
 
 const mapState = state => {
   return {
@@ -39,7 +109,17 @@ const mapState = state => {
   };
 };
 
-export default connect(
-  mapState,
-  null
-)(Auth);
+const mapDispatch = dispatch => {
+  return {
+    savePath(path) {
+      dispatch(loginActionCreators.savePath(path));
+    }
+  };
+};
+
+export default withRouter(
+  connect(
+    mapState,
+    mapDispatch
+  )(PriveateRoute)
+);
